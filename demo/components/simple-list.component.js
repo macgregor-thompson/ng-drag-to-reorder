ea03df00faa39815c55206bf4bfae3ca6723bab8
mhthompson86 <jackson86>
2017-03-24 15:43:02 -0400

    initial commit with a simple list demo

---
+++ demo/components/simple-list.component.js
@@ -0,0 +1,81 @@
+(function () {
+  'use strict';
+
+  angular.module('Demo')
+    .component('simpleList', {
+      template: `
+        <div class="panel panel-primary">
+          <div class="panel-heading">
+            <h4 class="panel-title">List of Avengers</h4>
+          </div>
+          <div class="panel-body">
+            <ul class="list-group">
+              <li class="list-group-item" ng-repeat="avenger in $ctrl.avengers" 
+                drag-to-reorder="$ctrl.avengers">
+                <span ng-bind="avenger.rank"></span>
+                <span ng-bind="avenger.name"></span>
+              </li>
+            </ul>
+          </div>
+          <div class="panel-footer">
+            <div class="input-group">
+              <input type="text" ng-model="$ctrl.character" class="form-control" placeholder="Add Marvel character..." ng-keyup="$ctrl.keyup($event)">
+              <span class="input-group-btn">
+                <button class="btn btn-primary" type="button" ng-click="$ctrl.add()">Add</button>
+              </span>
+            </div>
+          </div>
+        </div>
+        
+            `,
+      controller: listController
+    });
+
+  /* @ngInject */
+  function listController(ngDragToReorder, $scope) {
+    this.isSupported = ngDragToReorder.isSupported();
+
+    this.character = '';
+
+    this.avengers = [
+      {rank: 1, name: 'Thor'},
+      {rank: 2, name: 'Spider Man'},
+      {rank: 3, name: 'Captain America'},
+      {rank: 4, name: 'Hulk'},
+      {rank: 5, name: 'Iron Man'},
+      {rank: 6, name: 'Black Widow'},
+      {rank: 7, name: 'Ant Man'},
+      {rank: 8, name: 'Luke Cage'},
+      {rank: 9, name: 'Beast'},
+      {rank: 10, name: 'Wolverine'}
+    ];
+
+    //$scope.avengers = this.avengers;
+
+
+    $scope.$on('dragToReorder_drop', (e, data) => {
+      console.log('dropped event seen from component!');
+      console.log('data', data);
+      this.avengers = data.list.map((avenger, i) => {
+        avenger.rank = i + 1;
+        return avenger;
+      });
+    });
+
+
+    this.keyup = e => {
+      if (e.keyCode === 13) this.add();
+    };
+
+    this.add = () => {
+      if(this.character) {
+        this.avengers.push({
+          rank: this.avengers.length + 1,
+          name: this.character
+        });
+        this.character = '';
+      }
+    }
+
+  }
+})();
---
+++ demo/ngDragToReorder.js
@@ -0,0 +1,181 @@
+(function () {
+  'use strict';
+
+  angular.module('ngDragToReorder', [])
+  /* @ngInject */
+    .service('ngDragToReorder', function () {
+      return {
+        isSupported: draggable
+      };
+
+      function draggable() {
+        var div = document.createElement('div');
+        return ('draggable' in div) || ('ondragstart' in div && 'ondrop' in div);
+      }
+    })
+    /* @ngInject */
+    .directive('dragToReorder', function (ngDragToReorder, $parse) {
+      return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+
+          if (!ngDragToReorder.isSupported() || attrs.dragToReorder === '') return;
+
+          var el = element[0], list = $parse(attrs.dragToReorder)(scope), stringIdx, int, item,
+            newIdx, startIndex, target, offsetY, dragging = 'dtr-dragging', over = 'dtr-over',
+            droppingAbove = 'dtr-dropping-above', droppingBelow = 'dtr-dropping-below', transition = 'dtr-transition',
+            delay = 1500, loaded = false;
+
+          //console.log('list:', list);
+
+          if (attrs.hasOwnProperty('dtrInit')) {
+            attrs.$observe('dtrInit', function (val) {
+              if (val && val !== 'false') {
+                addListeners();
+              } else if (loaded) {
+                removeListeners();
+              }
+              loaded = true;
+            });
+          } else {
+            addListeners();
+          }
+
+          if (attrs.hasOwnProperty('dtrTransitionTimeout')) {
+            int = parseInt(attrs.dtrTransitionTimeout, 10);
+            delay = int ? int : 1000;
+          }
+
+
+          function addListeners() {
+            el.draggable = true;
+            el.addEventListener('dragstart', dragStart, false);
+            el.addEventListener('dragend', dragEnd, false);
+            el.addEventListener('dragenter', dragEnter, false);
+            el.addEventListener('dragleave', dragLeave, false);
+            el.addEventListener('dragover', dragOver, false);
+            el.addEventListener('drop', drop, false);
+          }
+
+          function removeListeners() {
+            el.draggable = false;
+            el.removeEventListener('dragstart', dragStart, false);
+            el.removeEventListener('dragend', dragEnd, false);
+            el.removeEventListener('dragenter', dragEnter, false);
+            el.removeEventListener('dragleave', dragLeave, false);
+            el.removeEventListener('dragover', dragOver, false);
+            el.removeEventListener('drop', drop, false);
+          }
+
+
+          function drop(e) {
+            e.preventDefault();
+            if (e.stopPropagation) e.stopPropagation();
+            startIndex = parseInt(e.dataTransfer.getData('text'), 10);
+            this.classList.remove(over);
+            if (this.classList.contains(droppingAbove)) {
+              if (startIndex < scope.$index) {
+                newIdx = scope.$index - 1;
+              } else {
+                newIdx = scope.$index;
+              }
+            } else {
+              if (startIndex < scope.$index) {
+                newIdx = scope.$index;
+              } else {
+                newIdx = scope.$index + 1;
+              }
+            }
+
+            item = list.splice(startIndex, 1)[0];
+            list.splice(newIdx, 0, item);
+
+            this.classList.remove(over);
+            this.classList.remove(droppingAbove);
+            this.classList.remove(droppingBelow);
+            if (this.previousElementSibling) {
+              this.previousElementSibling.classList.remove(over);
+              this.previousElementSibling.classList.remove(droppingAbove);
+              this.previousElementSibling.classList.remove(droppingBelow);
+            }
+            if (this.nextElementSibling) {
+              this.nextElementSibling.classList.remove(over);
+              this.nextElementSibling.classList.remove(droppingAbove);
+              this.nextElementSibling.classList.remove(droppingBelow);
+            }
+
+           scope.$apply(function() {
+             scope.$emit('dragToReorder_drop', {
+               item: item,
+               newIndex: newIdx,
+               prevIndex: startIndex,
+               list: list
+             });
+           })
+          }
+
+
+          function dragStart(e) {
+            e.dataTransfer.effectAllowed = 'move';
+            // **IMPORTANT** In order for this to work in IE, 'text' MUST
+            // not 'Text', ot 'text/plain' etc...
+            // Also the value (idx) needs to be a string
+            stringIdx = scope.$index.toString();
+            e.dataTransfer.setData('text', stringIdx);
+            this.classList.add(dragging);
+            this.classList.add(transition);
+            return false;
+          }
+
+          function dragEnd(e) {
+            target = this;
+            target.classList.remove(dragging);
+            setTimeout(function () {
+              target.classList.remove(transition)
+            }, delay);
+            return false;
+          }
+
+
+          function dragOver(e) {
+            e.preventDefault();
+            e.dataTransfer.dropEffect = 'move';
+            offsetY = e.offsetY;
+            if (offsetY < (this.offsetHeight / 2)) {
+              this.classList.remove(droppingBelow);
+              this.classList.add(droppingAbove);
+              if (this.previousElementSibling)
+                this.previousElementSibling.classList.add(droppingBelow);
+              if (this.nextElementSibling)
+                this.nextElementSibling.classList.remove(droppingAbove);
+            } else {
+              this.classList.remove(droppingAbove);
+              this.classList.add(droppingBelow);
+              if (this.previousElementSibling)
+                this.previousElementSibling.classList.remove(droppingBelow);
+              if (this.nextElementSibling)
+                this.nextElementSibling.classList.add(droppingAbove);
+            }
+            return false;
+          }
+
+          function dragEnter(e) {
+            e.preventDefault();
+            if (!this.classList.contains(dragging))
+              this.classList.add(over);
+          }
+
+          function dragLeave(e) {
+            this.classList.remove(over);
+            this.classList.remove(droppingAbove);
+            this.classList.remove(droppingBelow);
+            return false;
+          }
+
+
+        }
+      };
+
+    });
+
+})();
